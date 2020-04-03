d8579c0930364ace0ed6799eadb4465363d5992d
GuRuGu <peter.k.nets@gmail.com>
2020-04-03 11:17:56 +0300

    Add margins and sizes

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("booo");
+const dataUrl =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+const margins = { top: 20, right: 20, bottom: 70, left: 70 };
+const width = 1000 - margins.left - margins.right;
+const height = 800 - margins.top - margins.bottom;
+
+d3.json(dataUrl).then(data => {
+  console.log(data);
+});
